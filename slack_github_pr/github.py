caaa33059b1af5da69e59017add594ecf2619418
Romain Pomier <romain@cardiologs.com>
2018-05-24 12:21:29 +0200

    Refactor Slack message building

--- slack_github_pr/github.py
+++ slack_github_pr/github.py
@@ -12,11 +12,24 @@ class GithubHandler(object):
         self.sender = self.payload.get('sender', {}).get('login')
         self.issue = self.payload.get('issue', {})
         self.pull_request = self.payload.get('pull_request', {})
+        self.object_type = 'PR' if 'pull_request' in self.issue else 'issue'
         self.users = []
         self.message = None
 
+        if self.pull_request:
+            self.object_type = 'PR'
+            self.object = self.pull_request
+        elif self.issue:
+            self.object_type = 'issue'
+            self.object = self.issue
+
+    def build_message(self, action_desc):
+        self.message = "{} {} {} #{}: {}. {}".format(
+            self.sender, action_desc, self.object_type, self.object['number'],
+            self.object['title'], self.object['html_url'])
+
     def handle(self):
-        if self.sender in app.config['GITHUB_IGNORED_USERS']:
+        if self.object is None or self.sender in app.config['GITHUB_IGNORED_USERS']:
             # Ignore the current action
             return [], ""
         if self.action == 'review_requested':
@@ -24,51 +37,38 @@ class GithubHandler(object):
         elif self.event_type == 'pull_request_review' and self.action == 'submitted':
             self.handle_review_submitted()
         elif self.action == 'synchronize':
-            self.handle_pull_request_update('updated')
+            self.handle_object_update('updated')
         elif self.event_type == 'pull_request' and self.action == 'closed':
-            self.handle_pull_request_update('closed')
+            self.handle_object_update('closed')
         elif self.event_type == 'issues' and self.action == 'closed':
-            self.handle_issue_update('closed')
-        elif self.event_type == 'issues' and self.action == 'assigned':
-            self.handle_issue_assigned()
+            self.handle_object_update('closed')
+        elif self.action == 'assigned':
+            self.handle_assigned()
         elif self.event_type == 'issue_comment' and self.action == 'created':
-            self.handle_issue_update('commented on')
+            self.handle_object_update('commented on')
         to_notify = set(user['login'] for user in self.users if user['login'] != self.sender)
         return to_notify, self.message
 
     def handle_review_requested(self):
         self.users = [self.payload['requested_reviewer']]
-        self.message = "{} requested you to review PR #{}: {}. {}".format(
-            self.sender, self.pull_request['number'], self.pull_request['title'],
-            self.pull_request['html_url'])
+        self.build_message('requested you to review')
+
+    def handle_assigned(self):
+        self.users = [self.payload['assignee']]
+        self.build_message('assigned to you')
+
+    def handle_object_update(self, action_desc):
+        self.users = [self.object['user']]
+        self.users += self.object.get('requested_reviewers', [])
+        self.users += self.object.get('assignees', [])
+        self.build_message(action_desc)
 
     def handle_review_submitted(self):
         state = self.payload['review']['state']
         if state == "commented":
-            result = "commented on"
+            action_desc = "commented on"
         elif state == "changes_requested":
-            result = "requested changes on"
+            action_desc = "requested changes on"
         else:
-            result = "approved"
-        self.users = self.pull_request['requested_reviewers'] + [self.pull_request['user']]
-        self.message = "{} {} PR #{}: {}. {}".format(
-            self.sender, result, self.pull_request['number'], self.pull_request['title'],
-            self.pull_request['html_url'])
-
-    def handle_pull_request_update(self, update_type):
-        self.users = self.pull_request['requested_reviewers'] + [self.pull_request['user']]
-        self.message = "{} {} PR #{}: {}. {}".format(
-            self.sender, update_type, self.pull_request['number'],
-            self.pull_request['title'], self.pull_request['html_url'])
-
-    def handle_issue_assigned(self):
-        self.users = [self.payload['assignee']]
-        self.message = "{} assigned to you issue #{}: {}. {}".format(
-            self.sender, self.issue['number'], self.issue['title'], self.issue['html_url'])
-
-    def handle_issue_update(self, update_type):
-        object_type = 'PR' if 'pull_request' in self.issue else 'issue'
-        self.users = self.issue['assignees'] + [self.issue['user']]
-        self.message = "{} {} {} #{}: {}. {}".format(
-            self.sender, update_type, object_type, self.issue['number'],
-            self.issue['title'], self.issue['html_url'])
+            action_desc = "approved"
+        self.handle_object_update(action_desc)
